d3b7b282fdcf72290016ad4090b6cec53b2d9677
Your Name <tyrand3.lin@gmail.com>
2019-02-23 16:25:06 +0700

    displaying a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+
+    { id : 1, name : "Windstorm"},
+    { id : 2, name : "Windstorm2"},
+    { id : 3, name : "Windstorm3"},
+    { id : 4, name : "Windstorm4"},
+    { id : 5, name : "Windstorm5"},
+    { id : 6, name : "Windstorm6"},
+    { id : 7, name : "Windstorm7"},
+    { id : 8, name : "Windstorm8"},
+    { id : 9, name : "Windstorm9"},
+    { id : 10, name : "Windstorm10"}
+];
+    
